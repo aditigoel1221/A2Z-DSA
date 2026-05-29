057e091a4b3264b5a81241e5d1adc0a062692399
Aditi Goel <2aditigoel@gmail.com>
2026-05-29 18:36:18 +0530

    Pattern 1 Rectangular Star Pattern

---
+++ Patterns/Pattern-1 Rectangular Star Pattern.cpp
@@ -0,0 +1,14 @@
+// Problem Link: https://www.codingninjas.com/codestudio/problems/rectangular-star-pattern_893110?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+class Solution {
+public:
+    void pattern1(int n) {
+        for(int i = 1 ; i<=n ; i++){
+            for(int j = 1 ; j<=n ; j++){
+                cout<<"*";
+            }
+            cout<<"\n";
+        }
+
+    }
+};
